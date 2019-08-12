6664b85003d905f5d4eb6e4b59eb77326e95e8cf
robinsonking <918810978@qq.com>
2019-08-12 15:12:38 +0800

    update  passphrase style

--- app/components/inputs/PassphraseModalField.js
+++ app/components/inputs/PassphraseModalField.js
@@ -1,5 +1,5 @@
 export default ({label, children}) =>
   <div className="passphrase-modal-field">
-    <div className="passphrase-modal-field-label">{label}:</div>
+    <div className="passphrase-modal-field-label">{label}：</div>
     <div className="passphrase-modal-field-value">{children}</div>
   </div>;
